2d45c1fb7489e0f01f503a911ceb43e74ff17559
Eduarda <eduardabpetry@gmail.com>
2024-01-19 18:35:54 +0100

    Fix id declaration

--- Model/Article.php
+++ Model/Article.php
@@ -4,19 +4,27 @@ declare(strict_types=1);
 
 class Article
 {
+    public int $id;
     public string $title;
     public ?string $description;
     public ?string $publishDate;
 
-    public function __construct(string $title, ?string $description, ?string $publishDate)
+    public function __construct(int $id, string $title, ?string $description, ?string $publishDate)
     {
+        $this->id = $id;
         $this->title = $title;
         $this->description = $description;
         $this->publishDate = $publishDate;
     }
 
-    public function formatPublishDate($format = 'DD-MM-YYYY')
+    public function formatPublishDate($format = 'd-m-y')
     {
-        // TODO: return the date in the required format
+        if ($this->publishDate !== null) {
+            $dateTime = new DateTime($this->publishDate);
+
+            return $dateTime->format($format);
+        }
+
+        return '';
     }
 }
