58d8a0fe9c3b0c657afe37df4af7ce884ce75b64
Eduarda <eduardabpetry@gmail.com>
2024-01-19 10:19:53 +0100

    Edit getArticles function to connect and query the database

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -4,6 +4,14 @@ declare(strict_types = 1);
 
 class ArticleController
 {
+
+    private DatabaseManager $databaseManager;
+
+    public function __construct(DatabaseManager $databaseManager)
+    {
+        $this->databaseManager = $databaseManager;
+    }
+
     public function index()
     {
         // Load all required data
@@ -13,13 +21,19 @@ class ArticleController
         require 'View/articles/index.php';
     }
 
-    // Note: this function can also be used in a repository - the choice is yours
     private function getArticles()
     {
-        // TODO: prepare the database connection
-        // Note: you might want to use a re-usable databaseManager class - the choice is yours
-        // TODO: fetch all articles as $rawArticles (as a simple array)
-        $rawArticles = [];
+    
+        try {
+            $statement = $this->databaseManager->connection->prepare("SELECT * FROM articles");
+            $statement->execute();
+            $rawArticles = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+            return $rawArticles;
+        } catch (PDOException $error) {
+            echo $error->getMessage();
+            return [];
+        }
 
         $articles = [];
         foreach ($rawArticles as $rawArticle) {
